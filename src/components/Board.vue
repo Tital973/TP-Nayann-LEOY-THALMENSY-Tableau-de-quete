238accc3df6f346ef9d36b2efe1437d0c07410e0
Tital973 <nayann.leroy.thalmency@gmail.com>
2026-06-20 16:12:44 +0200

    finalisation du TP

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -9,11 +9,17 @@ export default {
     props: {
         colonnes: Array,
         quetes: Array
+    },
+    computed: {
+        quetesTerminees() {
+        return this.quetes.filter(q => q.statut === "Terminée").length;
     }
+}
 }
 </script>
 
 <template>
+    <p>Quêtes terminées : {{ quetesTerminees }}</p>
     <div class="tab">
         <Column v-for="colonne in colonnes" :key="colonne" :colonne="colonne" :quetes="quetes.filter(q => q.statut === colonne)"
             @select-quete="$emit('select-quete', $event)" @changer-statut="(quete, nouveauStatut) => $emit('changer-statut', quete, nouveauStatut)"
